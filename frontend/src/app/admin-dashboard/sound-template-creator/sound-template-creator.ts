eb9494e1a41b97ee074fe246e7828b41c716eddd
Vincent Tea <v.tea1@students.htl-leonding.ac.at>
2025-06-18 21:35:51 +0200

    fix syntax error

--- frontend/src/app/admin-dashboard/sound-template-creator/sound-template-creator.ts
+++ frontend/src/app/admin-dashboard/sound-template-creator/sound-template-creator.ts
@@ -6,7 +6,7 @@ import { Rarity, RaritySchema } from '../../../core/util/zod-schemas';
 import {MatButton} from '@angular/material/button';
 import {FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
 import {toSignal} from '@angular/core/rxjs-interop';
-import {SnackbarService} from '../../../core/services/snackbar-service';i
+import {SnackbarService} from '../../../core/services/snackbar-service';
 import {ConfigService} from '../../../core/services/config-service';
 import {NewSoundTemplate, SoundTemplateService} from '../../../core/services/sound-template-service';
 import {SoundFile, SoundFileService} from '../../../core/services/sound-file-service';
