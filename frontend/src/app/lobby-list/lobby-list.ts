080ed82765eb95d8d7ede0e2c20355911e05e93a
Paul Schein <p.schein@students.htl-leonding.ac.at>
2025-06-18 13:01:52 +0200

    added filter and name field for lobby creation

--- frontend/src/app/lobby-list/lobby-list.ts
+++ frontend/src/app/lobby-list/lobby-list.ts
@@ -1,10 +1,22 @@
-import {Component, computed, inject, OnDestroy, OnInit, Signal, signal, WritableSignal} from '@angular/core';
+import {
+  Component,
+  computed,
+  inject, model, ModelSignal,
+  OnDestroy,
+  OnInit,
+  Signal,
+  signal,
+  WritableSignal
+} from '@angular/core';
 import {Lobby, LobbyService} from '../../core/lobby.service';
 import {Subscription} from 'rxjs';
 import {MatProgressBar} from '@angular/material/progress-bar';
 import {LobbyCard} from './lobby-card/lobby-card';
 import {MatButton} from '@angular/material/button';
 import {Router, RouterLink} from '@angular/router';
+import {MatInput} from '@angular/material/input';
+import {MatFormField, MatLabel} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-lobby-list',
@@ -12,7 +24,11 @@ import {Router, RouterLink} from '@angular/router';
     MatProgressBar,
     LobbyCard,
     MatButton,
-    RouterLink
+    RouterLink,
+    MatFormField,
+    MatLabel,
+    FormsModule,
+    MatInput
   ],
   templateUrl: './lobby-list.html',
   styleUrl: './lobby-list.scss'
@@ -22,13 +38,31 @@ export class LobbyList implements OnInit, OnDestroy {
   protected readonly isLoading: WritableSignal<boolean> = signal(true);
   protected readonly noLobbies: Signal<boolean> = computed(() =>
     !(this.isLoading()) && this.lobbies().length === 0);
+  protected readonly newLobbyName: ModelSignal<string> = model<string>("");
+  protected readonly createBtnDisabled: Signal<boolean> = computed(() => {
+    const name: string | undefined = this.newLobbyName();
+    if (name === undefined) {
+      return true;
+    }
+    return name.trim() === "" || this.isLoading();
+  });
+  protected readonly filter: ModelSignal<string> = model<string>("");
+  protected readonly filteredLobbies: Signal<Lobby[]> = computed(() => {
+    let filter: string | undefined = this.filter();
+    if (filter === undefined) {
+      return this.lobbies();
+    }
+    filter = filter.trim().toLowerCase();
+    return this.lobbies().filter(lobby =>
+      lobby.name.toLowerCase().includes(filter));
+  });
 
   private readonly lobbyService: LobbyService = inject(LobbyService);
   private readonly subscriptions: Subscription[] = [];
   private readonly router = inject(Router);
 
   protected async handleCreateLobby(): Promise<void> {
-    const lobby: Lobby | null = await this.lobbyService.createLobby("test");
+    const lobby: Lobby | null = await this.lobbyService.createLobby(this.newLobbyName());
     if (lobby === null) {
       console.error('Failed to create lobby');
       return;
@@ -58,6 +92,7 @@ export class LobbyList implements OnInit, OnDestroy {
             return lobby;
           }));
       }));
+    this.newLobbyName.set("New Lobby");
     this.isLoading.set(false);
   }
 
