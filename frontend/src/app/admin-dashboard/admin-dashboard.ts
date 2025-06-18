374ded895e60c7e3985cae27dd4e322ee6965eee
Vincent Tea <v.tea1@students.htl-leonding.ac.at>
2025-06-18 22:26:56 +0200

    redesign admin dashboard

--- frontend/src/app/admin-dashboard/admin-dashboard.ts
+++ frontend/src/app/admin-dashboard/admin-dashboard.ts
@@ -2,13 +2,21 @@ import { Component } from '@angular/core';
 import {FileUpload} from './file-upload/file-upload';
 import {CaseTemplateCreator} from "./case-template-creator/case-template-creator";
 import {SoundTemplateCreator} from './sound-template-creator/sound-template-creator';
+import {MatTab, MatTabGroup} from '@angular/material/tabs';
+import {MatCard, MatCardContent, MatCardHeader, MatCardTitle} from '@angular/material/card';
 
 @Component({
   selector: 'app-admin-dashboard',
   imports: [
     FileUpload,
     CaseTemplateCreator,
-    SoundTemplateCreator
+    SoundTemplateCreator,
+    MatTabGroup,
+    MatTab,
+    MatCard,
+    MatCardHeader,
+    MatCardTitle,
+    MatCardContent
   ],
   templateUrl: './admin-dashboard.html',
   styleUrl: './admin-dashboard.scss'
