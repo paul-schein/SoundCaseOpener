dad9b51df55e783eb63ad132bf71f3d34f667ca4
paul <paul>
2025-06-18 23:37:45 +0200

    fixed bundle size by lazzy loading admin dashboard

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -6,9 +6,12 @@ import {authGuard, isAdminAuthGuard} from './auth-guard';
 import {LobbyList} from './lobby-list/lobby-list';
 import {LobbyDetail} from './lobby-list/lobby-detail/lobby-detail';
 
+const adminDashboardImport = () =>
+  import('./admin-dashboard/admin-dashboard').then(m => m.AdminDashboard);
+
 export const routes: Routes = [
   {path: 'home', component: Home, canActivate: [authGuard]},
-  {path: 'admin-dashboard', component: AdminDashboard, canActivate: [authGuard, isAdminAuthGuard] },
+  {path: 'admin-dashboard', loadComponent: adminDashboardImport, canActivate: [authGuard, isAdminAuthGuard] },
   {path: 'login', component: Login},
   {path: 'lobby-list', component: LobbyList, canActivate: [authGuard]},
   {path: 'lobby/:lobbyId', component: LobbyDetail, canActivate: [authGuard]},
